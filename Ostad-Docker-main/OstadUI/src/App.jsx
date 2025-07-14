8cee4c0d1870e81fbd6149e0f4e282215cf7e43c
Ashraful Azad <ashraful.azad86@gmail.com>
2025-07-14 22:09:15 +0600

    new server

--- Ostad-Docker-main/OstadUI/src/App.jsx
+++ Ostad-Docker-main/OstadUI/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
   // Fetch registered students
   const fetchStudents = async () => {
     try {
-      const response = await fetch("http://51.21.254.30:5050/getStudents");
+      const response = await fetch("http://51.20.119.161:5050/getStudents");
       const data = await response.json();
       setStudents(data);
     } catch (error) {
@@ -45,7 +45,7 @@ const handleSubmit = async (e) => {
 
   // Use toast.promise
   toast.promise(
-    fetch("http://51.21.254.30:5050/addStudent", {
+    fetch("http://51.20.119.161:5050/addStudent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
